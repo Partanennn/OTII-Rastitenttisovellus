05298349de94209d04266ea57470ba9821f7b66d
joonav <44670910+joonav@users.noreply.github.com>
2019-03-04 18:07:16 +0200

    Komponentit omiin js tiedostoihin.

---
+++ myapp/src/MyApp.js
@@ -0,0 +1,88 @@
+import React from "react"
+
+
+
+
+function MyApp() {
+    return (
+        <div className="container text-center text-md-left">
+            <div className="row mx-auto">
+                <div className="col-lg">
+                    <input type ="file" id="paskafilu"></input>
+                    <button type="button" className="btn btn-success">Import</button>
+                </div>
+            </div>
+            <div className="row mx-auto">
+                <div className="col-lg">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Luokka</th>
+                                <th>Opiskelija</th>
+                                <th>Kurssi</th>
+                                <th>Opettaja</th>
+                                <th>Tenttityyppi</th>
+                                <th>Lisätiedot</th>
+                                <th>Kampus</th>
+                            
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>ETA18SP</td>
+                                <td>Tekulainen Jonne</td>
+                                <td>KHP6969 Käsienheiluttelun perusteet</td>
+                                <td>Teku Ope</td>
+                                <td>Heiluttelu</td>
+                                <td>Käsien</td>
+                                <td>Kuopio</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div className="row mx-auto">
+                <div className="col-lg">
+                <button type="button" className="btn btn-success">Divide students</button>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Tila</th>
+                                <th>Valvoja</th>
+                                <th>Opiskelija</th>
+                                <th>Kurssi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>A-2069</td>
+                                <td>Make</td>
+                                <td>Tekulainen Jonne</td>
+                                <td>KHP</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className="col-lg">
+                    <button type="button" className="btn btn-success">Contact</button>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Prio</th>
+                                <th>Nimi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Make</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MyApp
---
+++ myapp/src/NavApp.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+
+function NavApp() {
+    return (
+        <nav className="navbar navbar-expand-md navbar-light navbar-fixed-top">
+            <a className="navbar-brand" href=""> <img src="savonia_logo.png" width="260" height="80"/></a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="">Paska</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default NavApp
