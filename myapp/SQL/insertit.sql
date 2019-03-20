07feea317b7d5b15a8da7cef42146aac8522acda
Aleksi Partanen <44446649+Partanennn@users.noreply.github.com>
2019-03-20 09:27:18 +0200

    ..

--- myapp/SQL/insertit.sql
+++ myapp/SQL/insertit.sql
@@ -2,7 +2,6 @@
 INSERT INTO data(luokka, opiskelija, kurssi, opettaja, tenttityyppi, tentin_lisatiedot, kampus) VALUES("ERA17SP", "opiskelija1", "4 ECF4000 Tekniikan fysiikka", "Laasanen Mikko Sakari", "Uusintatentti", "", "");
 
 /* Opettaja insertit */
-INSERT INTO teacher(name, priority, email) VALUES("Keke", 1, "keke@savonia.fi");
 INSERT INTO teacher(name, priority, email) VALUES("Opettaja1", 1, "Opettaja1@savonia.fi");
 INSERT INTO teacher(name, priority, email) VALUES("Opettaja3", 2, "Opettaja3@savonia.fi");
 INSERT INTO teacher(name, priority, email) VALUES("Opettaja2", 3, "Opettaja2@savonia.fi");
